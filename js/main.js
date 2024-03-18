4282e05517470357ab281a94bc2fb09080dbfe06
Mignon Stephane Dawson <3441356+m1nni3@users.noreply.github.com>
2024-03-19 01:36:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+    // Get references to the button and form container
+    const joinNowButton = document.getElementById("joinNowButton");
+    const registrationFormContainer = document.getElementById("registrationFormContainer");
+
+    // Add event listener to the "Join Now" button
+    joinNowButton.addEventListener("click", function () {
+        // Toggle the visibility of the form container
+        if (registrationFormContainer.style.display === "none") {
+            registrationFormContainer.style.display = "block";
+        } else {
+            registrationFormContainer.style.display = "none";
+        }
+    });
+
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+})(jQuery);
+
